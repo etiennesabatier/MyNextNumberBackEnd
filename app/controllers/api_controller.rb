b0c523b6d5fac82984f8498b2c5f9974d9092417
Etienne SABATIER <etiennesabatier@gmail.com>
2015-07-10 20:12:35 -0400

    release on Heroku

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,10 +2,10 @@ class ApiController < ApplicationController
 
 	def askNextNumber
 		if params && params[:requested_number]
-			myRequestedNumber = params[:requested_number]
-			@myNumber = Number.where(number: myRequestedNumber).first
-
-			render :json => @myNumber
+		myRequestedNumber = params[:requested_number]
+		@myNumber = Number.where(reference_number: myRequestedNumber).first
+		puts @myNumber.follower
+		render :json => @myNumber.to_json
 		end
 	end
 
---
+++ test.rb
@@ -0,0 +1,2 @@
+myNumber = Number.find_by(reference_number: "56").first
+return myNumber
